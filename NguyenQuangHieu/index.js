9aef90edf897c08c9c74e09daf303634458ed172
hvgiang1998@gmail.com <hvgiang10>
2018-12-07 12:10:19 +0700

    Màn hình câu hỏi
    hoàn thành

---
+++ NguyenQuangHieu/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import CauHoi from './CauHoi';
+import CauHoi2 from './CauHoi2';
+import CauHoi3 from './CauHoi3';
+import CauHoi4 from './CauHoi4';
+AppRegistry.registerComponent(appName, () => App);
